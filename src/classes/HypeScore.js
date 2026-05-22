5c9ff0b197d9cf924e51151f23ef4a7ab2bdcddf
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2026-05-22 12:44:33 +0000

    delete attribut useless

--- src/classes/HypeScore.js
+++ src/classes/HypeScore.js
@@ -3,17 +3,15 @@ import Logger from './logger.js'
 
 export class HypeScore {
 	constructor (game) {
-		this.game = game
 		this.logger = new Logger()
 	}
 
-	
-	computeHypeScore = (leagueName, team1Id, team2Id, lastWinner) => {
+	computeHypeScore = (gameName, leagueName, team1Id, team2Id, lastWinner) => {
 		try {
-			const gameScoreHype = gamesHype.includes(this.game) ? 1 : 0
+			const gameScoreHype = gamesHype.includes(gameName) ? 1 : 0
 			const leagueScoreHype = leaguesHype.includes(leagueName) ? 1 : 0
 			const teams1ScoreHype = lastWinner.length === 0 ? 0 : lastWinner.includes(team1Id) ? 1 : 0
-			const teams2ScoreHype = lastWinner.length === 0 ? 0 : lastWinner.includes(team2Id) ? 1 : 0
+			const teams2ScoreHype = lastWinner.length === 0 ? 0 : lastWinner.includes(team1Id) ? 1 : 0
 
 			const hypeScoreTotal = gameScoreHype + leagueScoreHype + teams1ScoreHype + teams2ScoreHype
 			return hypeScoreTotal === 4 ? 3 : hypeScoreTotal
