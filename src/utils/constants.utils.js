0c9e9dafe5f74d3e6909904c052a257e21f69c8d
Hugo Rodrigues <hugo38.rodrigues@gmail.com>
2026-08-05 11:53:52 +0000

    delete old folder

--- src/utils/constants.utils.js
+++
@@ -1,2 +0,0 @@
-export const TOKEN_API = process.env.TOKEN_API
-export const DB_ADAPTATER = process.env.DB_ADAPTATER
--- src/utils/hype-data.utils.js
+++
@@ -1,2 +0,0 @@
-export const gamesHype = ['League of legends']
-export const leaguesHype = ['LCK']
